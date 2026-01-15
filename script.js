4e1f9506438147a6c6216f9445a8d56d3fbc1477
unknown <abilenezepedaalmaraz@gmail.com>
2026-01-15 11:05:09 -0600

    Se realizo una postal para amigo

---
+++ script.js
@@ -0,0 +1,6 @@
+function mostrarMensaje(){
+    //*Obtenemos el elemento por su ID
+    let mensaje=document.getElementById("mensaje");
+    //*Cambiamos el texto del parrafo
+    mensaje.textContent ="Para que brilles hoy más que nunca. Disfruta tu mini spa (*ᴗ͈ˬᴗ͈)ꕤ*.ﾟ☕︎";
+}
